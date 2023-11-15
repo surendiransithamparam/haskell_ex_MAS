e76168cb47d295bddf0dd1e229c8afe2b48db658
Surendiran Sithamparam <surendiran@sithamparam.ch>
2023-11-15 15:12:09 +0100

    Update Exercises.hs
    solved Exercise 2

--- Ch02/Exercises.hs
+++ Ch02/Exercises.hs
@@ -9,7 +9,58 @@ module Exercises where
 -- below.
 
 {-
-TODO
+ghci> 2+3*4
+14
+ghci> (2+3)*4
+20
+ghci> sqrt (3^2 + 4^2)
+5.0
+ghci> head [1,2,3,4,5]
+1
+ghci> tail [1,2,3,4,5]
+[2,3,4,5]
+ghci> [1,2,3,4,5] !! 2
+3
+ghci> take 3 [1,2,3,4,5]
+[1,2,3]
+ghci> drop 3 [1,2,3,4,5]
+[4,5]
+ghci> length [1,2,3,4,5]
+5
+ghci> sum [1,2,3,4,5]
+15
+ghci> product [1,2,3,4,5]
+120
+ghci> [1,2,3] ++ [4,5]
+[1,2,3,4,5]
+ghci> reverse [1,2,3,4,5]
+[5,4,3,2,1]
+-}
+
+-- test.hs
+{-
+double x = x +x
+quadruple x = double (double x)
+factorial n = product [1..n]
+average ns = sum ns `div` length ns
+-}
+
+{-
+ghci Ch02/test.hs
+GHCi, version 9.4.7: https://www.haskell.org/ghc/  :? for help
+[1 of 2] Compiling Main             ( Ch02/test.hs, interpreted )
+Ok, one module loaded.
+ghci> quadruple 10
+40
+ghci> take (double 2) [1,2,3,4,5]
+[1,2,3,4]
+ghci> :r
+[1 of 2] Compiling Main             ( Ch02/test.hs, interpreted ) [Source file changed]
+Ok, one module loaded.
+ghci> factorial 10
+3628800
+ghci> average [1,2,3,4,5]
+3
 -}
 
 -------------------------------------------------------------------------------
@@ -20,10 +71,10 @@ TODO
 -- 2^3*4 2*3+4*5 2+3*4^5
 
 e_2_2_a :: Int
-e_2_2_a = {- TODO parenthesise -} 2^3*4
+e_2_2_a = 2 ^ (3 * 4)
 
 e_2_2_b :: Int
-e_2_2_b = {- TODO parenthesise -} 2*3+4*5
+e_2_2_b = (2 * 3) + (4 * 5)
 
 e_2_2_c :: Int
-e_2_2_c = {- TODO parenthesise -} 2+3*4^5
+e_2_2_c = 2 + (3 * (4 ^ 5))
